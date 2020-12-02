05d9d2debf8bf3a80849d7a536dfcad7c180c320
Hydro111 <hydrogenhone@gmail.com>
2020-12-02 15:47:34 -0700

    refactor ColorWheelSubsystem

--- src/main/java/frc/robot/subsystems/ColorWheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/ColorWheelSubsystem.java
@@ -24,173 +24,169 @@ import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
 public class ColorWheelSubsystem extends SubsystemBase {
-  /**
-   * Creates a new ExampleSubsystem.
-   */
+ /**
+  * Creates a new ExampleSubsystem.
+  */
   
-   private final WPI_TalonFX colorWheel;
-   private ColorSensorV3 colorSensor;
-   private DigitalInput touchSensor = new DigitalInput(0);
-   //private final WPI_TalonSRX slideMotor;
-
-    
-    private final ColorMatch colorMatcher = new ColorMatch();
-    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private final WPI_TalonFX colorWheel;
+  private ColorSensorV3 colorSensor;
+  private DigitalInput touchSensor = new DigitalInput(0);
+  //private final WPI_TalonSRX slideMotor;
+  
+  
+  private final ColorMatch colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
   
 
-    public ColorWheelSubsystem() {
+  public ColorWheelSubsystem() {
 
-        this.colorWheel = new WPI_TalonFX(ColorWheelConstants.kColorWheelPort);
-       // this.slideMotor = new WPI_TalonSRX(ColorWheelConstants.kSlideMotorPort);
-        this.colorSensor = new ColorSensorV3(I2C.Port.kOnboard); //todo: find sensor I2C address
-        colorMatcher.addColorMatch(kBlueTarget);
-        colorMatcher.addColorMatch(kGreenTarget);
-        colorMatcher.addColorMatch(kRedTarget);
-        colorMatcher.addColorMatch(kYellowTarget);    
+    this.colorWheel = new WPI_TalonFX(ColorWheelConstants.kColorWheelPort);
+    // this.slideMotor = new WPI_TalonSRX(ColorWheelConstants.kSlideMotorPort);
+    this.colorSensor = new ColorSensorV3(I2C.Port.kOnboard); //todo: find sensor I2C address
+    this.colorMatcher.addColorMatch(this.kBlueTarget);
+    this.colorMatcher.addColorMatch(this.kGreenTarget);
+    this.colorMatcher.addColorMatch(this.kRedTarget);
+    this.colorMatcher.addColorMatch(this.kYellowTarget);    
         
     }
 
-    public String getColor() {
-      Color detectedColor = colorSensor.getColor();
+  public String getColor() {
+    Color detectedColor = this.colorSensor.getColor();
 
-        String colorString;
-        ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+    String colorString;
+    ColorMatchResult match = this.colorMatcher.matchClosestColor(detectedColor);
     
-        if (match.color == kBlueTarget) {
-          colorString = "Blue";
-        } else if (match.color == kRedTarget) {
-          colorString = "Red";
-        } else if (match.color == kGreenTarget) {
-          colorString = "Green";
-        } else if (match.color == kYellowTarget) {
-          colorString = "Yellow";
-        } else {
-          colorString = "Unknown";
-        }
+    if (match.color == this.kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == this.kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == this.kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == this.kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
 
-        return colorString;
+      return colorString;
     }
 
     @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-        Color detectedColor = colorSensor.getColor();
+  public void periodic() {
+    // This method will be called once per scheduler run
+    Color detectedColor = this.colorSensor.getColor();
 
-        String colorString;
-        ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+    String colorString;
+    ColorMatchResult match = this.colorMatcher.matchClosestColor(detectedColor);
+  
+    if (match.color == this.kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == this.kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == this.kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == this.kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
     
-        if (match.color == kBlueTarget) {
-          colorString = "Blue";
-        } else if (match.color == kRedTarget) {
-          colorString = "Red";
-        } else if (match.color == kGreenTarget) {
-          colorString = "Green";
-        } else if (match.color == kYellowTarget) {
-          colorString = "Yellow";
-        } else {
-          colorString = "Unknown";
-        }
-    
-        /**
-         * Open Smart Dashboard or Shuffleboard to see the color detected by the 
-         * sensor.
-         */
-        SmartDashboard.putNumber("Red", detectedColor.red);
-        SmartDashboard.putNumber("Green", detectedColor.green);
-        SmartDashboard.putNumber("Blue", detectedColor.blue);
-        SmartDashboard.putNumber("Confidence", match.confidence);
-        SmartDashboard.putString("Detected Color", colorString);
+    /**
+     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
+     * sensor.
+     */
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
     }
 
-    public void setWheel(double power)
-    {
-      colorWheel.set(power);
+  public void setWheel(double power) {
+      this.colorWheel.set(power);
+  }
+  public boolean getTouching() {
+    return this.touchSensor.get();
+  }
+  public double getEncoderCount() {
+      return this.colorWheel.getSensorCollection().getIntegratedSensorAbsolutePosition();
+  }
+  public void resetEncoders() {
+      this.colorWheel.getSensorCollection().setIntegratedSensorPosition(0, 0);
+  }
+  public void spin() {
+      this.colorWheel.set(ColorWheelConstants.kColorWheelPower);
+  }
+  /*public void stop() {
+    this.colorWheel.set(0);
+    slideMotor.set(0);
+  }
+  public void extend() {
+    slideMotor.set(ColorWheelConstants.kSlideMotorPower);
+  }
+  public void retract() {
+    slideMotor.set(-ColorWheelConstants.kSlideMotorPower);
+  }
+  public void safeExtend() {
+    while(!getTouching()) {
+      extend();
     }
-    public boolean getTouching(){
-      return touchSensor.get();
+    stop();
+  }
+  public void safeRetract() {
+    while(!getTouching()) {
+      retract();
     }
-   
-    public double getEncoderCount(){
-        return colorWheel.getSensorCollection().getIntegratedSensorAbsolutePosition();
-    }
-    public void resetEncoders()
-    {
-        colorWheel.getSensorCollection().setIntegratedSensorPosition(0, 0);
-    }
-    public void spin()
-    {
-        colorWheel.set(ColorWheelConstants.kColorWheelPower);
-    }
-  /*  public void stop()
-    {
-        colorWheel.set(0);
-        slideMotor.set(0);
-    }
-    public void extend(){
-      slideMotor.set(ColorWheelConstants.kSlideMotorPower);
-    }
-    public void retract(){
-      slideMotor.set(-ColorWheelConstants.kSlideMotorPower);
-    }
-    public void safeExtend(){
-      while(!getTouching()){
-        extend();
+    stop();
+  }
+  //will spin to a color so that the correct color is lined up with the game color sensor
+  public void spinToColor(String color) {
+    if(color.equals("Red")) {
+      while(!getColor().equals("Blue")) {
+          spin();
       }
       stop();
     }
-    public void safeRetract(){
-      while(!getTouching()){
-        retract();
+    else if(color.equals("Blue")) {
+      while(!getColor().equals("Red")) {
+        spin();
       }
       stop();
     }
-    //will spin to a color so that the correct color is lined up with the game color sensor
-    public void spinToColor(String color){
-      if(color.equals("Red")){
-        while(!getColor().equals("Blue")){
-            spin();
-        }
-        stop();
+    else if(color.equals("Green")) {
+      while(!getColor().equals("Yellow")) {
+        spin();
       }
-      else if(color.equals("Blue")){
-        while(!getColor().equals("Red")){
-          spin();
-        }
-        stop();
-      }
-      else if(color.equals("Green")){
-        while(!getColor().equals("Yellow")){
-          spin();
-        }
-        stop();
-      }
-      else if(color.equals("Yellow")){
-        while(!getColor().equals("Green")){
-          spin();
-        }
-        stop();
-      }
+      stop();
     }
+    else if(color.equals("Yellow")) {
+      while(!getColor().equals("Green")) {
+        spin();
+      }
+      stop();
+    }
+  }
     
-    public void spinAmount(int times){
-      String color = getColor();
-      boolean countAgain=false;
-      int passCount = 0;
-      spin();
-      while(passCount<times*2){
-        if(!getColor().equals(color)){
-          countAgain=true;
-        }
-        else{
-          if(countAgain){
-            passCount++;
-            countAgain=false;
-          }
+  public void spinAmount(int times) {
+    String color = getColor();
+    boolean countAgain=false;
+    int passCount = 0;
+    spin();
+    while(passCount<times*2) {
+      if(!getColor().equals(color)) {
+        countAgain=true;
+      }
+      else{
+        if(countAgain) {
+          passCount++;
+          countAgain=false;
         }
       }
-      stop();
-    }*/
+    }
+    stop();
+  }*/
 }
